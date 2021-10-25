0ce0fb995caee0dfaf9e0b632aeeff3f18dbf06a
Ignacio <jakrich@cifpfbmoll.eu>
2021-10-25 16:38:50 +0200

    Creado el funcionamiento de las letras y numeros

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,33 @@
 //TODO Crear las funcionalidades del las teclas del teclado virtual
+const teclado = {
+    //TODO Funcionamiento de las teclas normales
+    init() {
+      const buttons = document.querySelectorAll(".button");
+  
+      buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+          document.getElementById("textarea").value += button.textContent;
+        });
+      });
+    },
+    //TODO Funcionamiento de la tecla enter
 
+    //TODO Funcionamiento de la tecla C
+
+    //TODO Funcionamiento de la tecla borrar ultima letra
+
+    //TODO Funcionamiento de la tecla borrar primera letra
+
+    //TODO Funcionamiento de la tecla CE
+
+    //TODO Funcionamiento de la tecla enviar
+
+    //TODO Funcionamiento de la tecla mayúsculas
+
+    //TODO Funcionamiento de la tecla de emojis
+ 
+  };
+  
+  window.addEventListener("DOMContentLoaded", function () {
+    teclado.init();
+  });
